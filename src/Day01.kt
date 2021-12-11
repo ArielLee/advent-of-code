b5f517eea8c4f24cc0087c18156ba3a073cf3235
Ariel Lee <ariellee@eztravel.com.tw>
2021-12-11 16:35:57 +0800

    day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,38 @@
 fun main() {
+    fun add3Int(input: List<String>, start: Int, end: Int): Int {
+        var sum = 0
+        for (i in start .. end) {
+            sum += input[i].toInt()
+        }
+        return sum
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var answer = 0
+        var previous = input[0].toInt()
+        for (num in input) {
+            if (previous < num.toInt()) answer++
+            previous = num.toInt()
+        }
+        return answer
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var answer = 0
+        var previous = add3Int(input, 0, 2)
+        for (i in 0 until input.size - 3) {
+            var next = add3Int(input, i + 1, i + 3)
+            if (previous < next) answer++
+            previous = next
+        }
+        return answer
     }
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
-    println(part1(input))
     println(part2(input))
 }
